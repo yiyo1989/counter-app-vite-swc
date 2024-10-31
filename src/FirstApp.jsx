7fadb38931cdc81835f3ddfbfcd30022884468eb
yiyo1989 <gbojorge@ccss.sa.cr>
2024-10-31 15:58:37 -0600

    yarn

---
+++ src/FirstApp.jsx
@@ -0,0 +1,31 @@
+
+
+
+
+function pruebas() {
+  return {
+    message: 'Hola',
+    title: 'Pagina prueba'
+    };
+}
+
+
+
+
+const FirstApp = ({title,subtitle}) => {
+ 
+  return (
+    <>
+     <h1>{title}</h1>
+     <h2>{subtitle}</h2>     
+     <p>Bojorges</p>
+    </>
+   
+  )
+}
+
+export default FirstApp
+
+
+
+
